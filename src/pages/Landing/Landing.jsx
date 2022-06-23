3d8bcd9097a028512828b11f965129e02882491d
Jan Horak <janny_b_goode@hotmail.com>
2022-06-22 22:57:39 -0400

    add popup update functionality

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,66 +1,16 @@
-import React, { useRef, useState, useEffect } from "react";
-
-import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-// import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
-// import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-import "mapbox-gl/dist/mapbox-gl.css";
+import React from "react";
 
 import "../../styles/Landing.css";
 
 //components
-
-import InputField from "../../components/GeoSearch/inputField.js";
-
-mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+import Map from "../../components/Map/Map";
 
 const Landing = () => {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
-  const marker = new mapboxgl.Marker({ draggable: true, color: "#FF0000" });
-  const nav = new mapboxgl.NavigationControl();
-  const scale = new mapboxgl.ScaleControl({
-    maxWidth: 80,
-    unit: 'imperial'
-    });
-  
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/janhorak/cl46d05kd004j14qt7avuchry",
-      center: [lng, lat],
-      zoom: zoom,
-    }).addControl(nav, 'bottom-left').addControl(scale);
-  });
-  
-  // map.addControl(nav, 'top-left')
-
-
-  const handleSearchResult = (results) => {
-    console.log("from input", results);
-    setLng(results[0]);
-    setLat(results[1]);
-    marker.setLngLat(results).addTo(map.current).setPopup(new mapboxgl.Popup().setHTML(`<h1>Testing POPUP!</h1><br/><h3>${results}</h3>`))
-    map.current.flyTo({
-      center: results,
-      zoom: 12,
-      essential: true,
-    });
-  };
-
-  const handleMarkerDragend = () => {
-    const lngLat = marker.getLngLat();
-  };
-
-  marker.on("dragend", handleMarkerDragend);
-
   return (
     <div>
-      <InputField handleSearchResult={handleSearchResult} />
-      <div ref={mapContainer} className="map-container"></div>
+      <div className="map-component">
+        <Map />
+      </div>
     </div>
   );
 };
